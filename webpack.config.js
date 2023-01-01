9b89be0c8f86190d31ce75fad7e6c43744a05a84
Moez <moeez.abdullah@farmdar.co.uk>
2023-01-01 22:20:57 +0500

    Added css for button and header & add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 const path = require('path')
 
 const postCSSPlugins = [
+    require('postcss-import'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
 ]
 
-module.exports = {
+module.exports = { 
     entry: './app/assets/scripts/APP.js',
     output: {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+        // before: function(app, server) {
+        //     server._watch('./app/**/*.html')
+        // },
+        watchFiles: ('./app/**/*.html'),
+        static: {
+            directory: path.join(__dirname, "app")
+          },
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
     mode: 'development',
-    watch: true,
     module : {
         rules : [
             {
