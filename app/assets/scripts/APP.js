387749cc836517dbadeba244125559028d1ac796
Moez <moeez.abdullah@farmdar.co.uk>
2023-01-04 20:22:39 +0500

    completed the overlay

--- app/assets/scripts/APP.js
+++ app/assets/scripts/APP.js
@@ -1,10 +1,12 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
-import StickyHeader from './modules/StickyHeader';
+import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
 // import Person from './modules/Person'
 
+new Modal();
 let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
