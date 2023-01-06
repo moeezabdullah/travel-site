a999dc5f2dd94f26df2a380e3cd41a59cc4803d2
Moez <moeez.abdullah@farmdar.co.uk>
2023-01-06 12:39:43 +0500

    created our first cloud function_1

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,4 @@
-export.handler = function(events, context, callback) {
+exports.handler = function(events, context, callback) {
     callback(null, {
         statusCode: 200,
         body: 'Welcome to the secret area'
