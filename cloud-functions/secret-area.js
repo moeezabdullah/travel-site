fb1e257b78a7ae337c84e8a287b52d1f98a2eab0
Moez <moeez.abdullah@farmdar.co.uk>
2023-01-09 01:39:40 +0500

    updated cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,31 @@
 exports.handler = function(events, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: 'Welcome to the secret area'
-    })
+    const secretContent = `
+    <h3>Welcome to the secret area</h3>
+    <p>Your password was correct</p>
+    `
+    
+    let body
+
+    if (events.body) {
+        body = JSON.parse(events.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+
+    } else {
+        callback(null, {
+            statusCode: 401
+            
+        })
+    }
+
+    
+    
+    
 }
